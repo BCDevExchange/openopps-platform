253b4577c42532c954d204f60f9a6b3ca109b617
Chris Coldwell <chris.coldwell@gmail.com>
2016-08-31 09:53:45 -0700

    reverting bootstrap to NOT insert user

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -15,6 +15,66 @@ var fs = require('fs'),
  */
 var buildDictionary = require('sails-build-dictionary');
 
+var createAdmin = function (cb) {
+  var name = 'Admin'
+    , username = 'admin@admin.com'
+    , password = 'admin1234'
+    , email = 'admin@admin.com'
+    , tags = ['admin'];
+
+  if (!username) {
+    req.flash('error', 'Error.Passport.Username.Missing');
+    return next(new Error('No username was entered.'));
+  }
+
+  if (!password) {
+    req.flash('error', 'Error.Passport.Password.Missing');
+    return next(new Error('No password was entered.'));
+  }
+
+  User.create ({
+    isAdmin : true,
+    username : email,
+    name: name,
+    tags: tags,
+    email: email
+  }, function (err, user) {
+    if (err) {
+      if (err.code === 'E_VALIDATION') {
+        req.flash('error', 'Error.Passport.User.Exists');
+      }
+
+      return next(err);
+    }
+
+    // Generating accessToken for API authentication
+    var token = crypto.randomBytes(48).toString('base64');
+
+    Passport.create ({
+      protocol    : 'local'
+    , password    : password
+    , user        : user.id
+    , accessToken : token
+    , email       : email
+    }, function (err, passport) {
+      if (err) {
+        if (err.code === 'E_VALIDATION') {
+          req.flash('error', 'Error.Passport.Password.Invalid');
+        }
+
+        return user.destroy(function (destroyErr) {
+          next(destroyErr || err);
+        });
+      }
+
+      next(null, user);
+    });
+  });
+};
+
+
+
+
 module.exports.bootstrap = function (cb) {
 
   // If upload directory is missing, create it
@@ -33,7 +93,18 @@ module.exports.bootstrap = function (cb) {
         caseSensitive : true
       }, function (err, modules) {
            sails.services = modules;
-           cb();
+
+           // User.create ({
+
+           // }).exec (function (err, newuser) {
+           //  if (err) {
+           //    console.log ('Error creating admin user:', err);
+           //  } else {
+           //    console.log ('created admin user:', newuser);
+           //  }
+            cb();
+           // });
+
          });
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
