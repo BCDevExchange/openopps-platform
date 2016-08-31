39cb962fab29a3b8759e411991db4d8302486b94
Chris Coldwell <chris.coldwell@gmail.com>
2016-08-31 10:32:23 -0700

    fix the sql in string to work correctly

--- migrations/20160830214134-add-admin.js
+++ migrations/20160830214134-add-admin.js
@@ -24,13 +24,14 @@ exports.up = function(db, callback) {
 	// callback();
 	var bcrypt = require('bcryptjs');
 	var password = process.env.ADMIN_PASSWORD;
+	console.log ('++++++++++++++ supplied password = ', password);
 	bcrypt.hash (password, 10, function (err, hash) {
+		var sql = 'insert into midas_user (username, "isAdmin", name, "isAgencyAdmin") values (\'admin@admin.com\', true, \'admin\', true);\n'+
+				'insert into passport (protocol, password, "user") values (\'local\', \''+hash+'\', currval(\'midas_user_id_seq\'));';
+		console.log (sql);
 		if (err) console.log (err);
 		else {
-			db.runSql (
-				"insert into midas_user (username, 'isAdmin', name, 'isAgencyAdmin') values ('admin@admin.com', true, 'admin', true);\n"+
-				"insert into passport (protocol, password, 'user') values ('local', '"+hash+"', currval('midas_user_id_seq'));"
-			, callback);
+			db.runSql (sql, callback);
 		}
 	});
 };
